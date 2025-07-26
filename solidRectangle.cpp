cf25c0ae4de22a584fdc98fde2389dc42174b1ca
Aditya Tiwari <aktiwari089081@gmail.com>
2025-07-26 14:56:34 +0530

    Patterns

---
+++ solidRectangle.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+using namespace std;
+
+// 1
+// int main(){
+//     int a;
+//     cout<<"no. of rows: ";
+//     cin>>a;
+//     int p = a;
+//     for(int i =1;i<=a;i++){
+//         for(int j =1;j<=p;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+    
+// }
+
+// 1
+// 21
+// 321
+// 4321
+// 54321
+// 2
+// int main(){
+//     int a;
+//     cout<<"no. of rows: ";
+//     cin>>a;
+//     // char ch ='A';
+//     for(int i =1;i<=a;i++){
+//         for(int j =1;j<=i;j++){
+//             cout<<i-j+1;
+//         }
+//         cout<<endl;
+//     }
+// }
+//      1
+//     22
+//    333
+//   4444
+//  55555
+// int main(){
+//     int a;
+//     cout<<"no. of rows: ";
+//     cin>>a;
+//     // char ch ='A';
+//     for(int i =1;i<=a;i++){
+//         for(int j =1;j<=a-i+1;j++){
+//             cout<<" ";
+//         }
+//         for(int k =1;k<=i;k++){
+//             cout<<i;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//  11111
+//   2222
+//    333
+//     44
+//      5
+// int main(){
+//     int a;
+//     cout<<"no. of rows: ";
+//     cin>>a;
+//     // char ch ='A';
+//     for(int i =1;i<=a;i++){
+//         for(int j =1;j<=i;j++){
+//             cout<<" ";
+//         }
+//         for(int k =1;k<=a-i+1;k++){
+//             cout<<i;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//     1
+//    121
+//   12321
+//  1234321
+// 123454321
+// int main(){
+
+//     int a =5;
+//     for(int i =1;i<=a;i++){
+//         for(int j =1;j<=a-i;j++){
+//             cout<<" ";
+//         }
+//         for(int k =1;k<=i;k++){
+//             cout<<k;
+//         }
+//         for(int l =1;l<i;l++){
+//             cout<<i-l;
+//         }
+//                or
+//         for(int l =i;l<1;l++){
+//             cout<<l;
+//         }
+//         cout<<endl;
+//     }
+// }
+// 3
